c1ca43a22f24cdc6a9ef86ae8cc50f2bef2350d8
networkgangster <abdussamed_koekdal@hotmail.de>
2020-10-15 13:29:14 +0200

    changed ports

--- nlb.tf
+++ nlb.tf
@@ -6,20 +6,19 @@ resource "exoscale_nlb" "projectwork_nlb" {
 
 resource "exoscale_nlb_service" "projectwork_nlb_service" {
   zone             = exoscale_nlb.projectwork_nlb.zone
-  name             = "projectwork-https"
-  description      = "Website over HTTPS"
+  name             = "projectwork-http"
+  description      = "Website over HTTP"
   nlb_id           = exoscale_nlb.projectwork_nlb.id
   instance_pool_id = exoscale_instance_pool.projectwork_instancepool.id
     protocol       = "tcp"
     port           = 80
-    target_port    = 8080
+    target_port    = 80
     strategy       = "round-robin"
 
   healthcheck {
-    mode     = "https"
-    port     = 8080
+    mode     = "http"
+    port     = 80
     uri      = "/health"
-    tls_sni  = "example.net"
     interval = 5
     timeout  = 3
     retries  = 1
